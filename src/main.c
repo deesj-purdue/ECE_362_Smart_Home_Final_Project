4f9e9b135a81054b4f6bc64248b9c252281ed666
deesj-purdue <deesj@purdue.edu>
2024-10-28 14:02:43 -0400

    global variables added

--- src/main.c
+++ src/main.c
@@ -8,6 +8,37 @@
  */
 
 #include "stm32f0xx.h"
+#include <stdlib.h>
+
+/* GLOBAL VARIABLES */
+
+float CURRENT_TEMPERATURE = 0; // Celsius
+float TARGET_TEMPERATURE = 0;  // Celsius
+int CORRECT_PASSWORD = 1234;   // 4 digit pin
+int FAN_SPEED = 0;             // 0 - 100% speed range
+enum DOOR_STATE
+{
+  OPEN,
+  CLOSED
+};
+enum SECURITY_STATE
+{
+  DISARMED, // Door open state will NOT set off alarm
+  ARMED, // Door open WILL set off alarm
+  PASSWORD_ENTRY, // Door has just been opened, correct password sends to DISARMED, incorrect password sends to ALARM
+  ALARM // buzzer turned on, will not disable unless correct password is entered
+};
+
+void init_tim1_buzzer_pwm()
+{
+  RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+  TIM1->PSC = 4799; // 48 MHz / 4800 = 10 kHz
+  TIM1->ARR = 9;    // 10 kHz / 10 = 1 kHz
+  TIM1->CCR1 = 499; // 50% duty cycle
+
+  TIM1->CR1 |= TIM_CR1_CEN;
+}
 
 int main()
 {
